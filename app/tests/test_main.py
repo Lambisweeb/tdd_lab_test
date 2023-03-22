40f0fd51b92c1f2be769dd7a731643f110adf40d
Lambisweeb <88651211+Lambisweeb@users.noreply.github.com>
2023-03-22 15:28:04 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,3 +9,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "Tay"}
+    
+def test_post_main():
+    response = client.post("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "Tay"}
