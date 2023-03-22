4709d8a917802fa9d4fb99a2155f5f2fb409e357
Lambisweeb <88651211+Lambisweeb@users.noreply.github.com>
2023-03-22 15:26:34 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,4 +12,13 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.post("/")
+def read_root():
+    return {"Hello": "Tay"}
+
+
+@app.post("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
 handler = Mangum(app)
